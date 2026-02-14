d34e46d7afa319233d50b185ed65a0c3f2bb0889
gabriel-bosio <gabriel.bosio@hotmail.com>
2026-02-14 20:38:47 -0300

    Feat: Busca randomizada

--- ProblemaTarefas-TeoriaDaComputacao/Controle.cs
+++ ProblemaTarefas-TeoriaDaComputacao/Controle.cs
@@ -10,20 +10,38 @@ namespace ProblemaTarefas_TeoriaDaComputacao
 
         private static double[] conjuntoR = { 1.5, 2.0 };
 
+        private static double[] conjuntoAlpha = { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9 };
+
         public static void Executar()
         {
             Random rand = new Random();
-            string caminhoSalvamento = @"C:\Projetos\Teoria da Computação\Resultados_Tarefas";
+            string caminhoSalvamento = @"C:\Projetos\Teoria da Computação\Resultados_Tarefas"; //Preencher com o caminho de salvamento desejado
             Directory.CreateDirectory(caminhoSalvamento);
             string nomeArquivo = $"Resultado_{DateTime.Now:dd_MM_yyyy_HH_mm}.txt";
             string caminhoCompleto = Path.Combine(caminhoSalvamento, nomeArquivo);
 
-            Console.WriteLine("Criando arquivo...");
             using (var writer = new StreamWriter(caminhoCompleto, append: false, encoding: Encoding.UTF8))
             {
                 writer.WriteLine("Heurística, n, m, replicação, tempo, iterações, makespan inicial, makespan final, parametro\n");
 
-                Console.WriteLine("Simulando buscas locais monótonas");
+                //Gera dados para primeira melhora, refazendo paramentros aleatorios por execução
+                for (int i = 0; i <= 10; i++)
+                {
+
+                    int m = conjuntoM[rand.Next(0, 3)];
+                    double r = conjuntoR[rand.Next(0, 2)];
+
+                    int n = (int)Math.Pow(m, r);
+
+                    ResolucaoTarefas resolucao = new ResolucaoTarefas(n, m);
+                    resolucao.ExecutaPrimeiraEscolha();
+
+                    writer.WriteLine($"Monótona primeira escolha, {n}, {m}, {i}, {resolucao.Tempo} ms, {resolucao.Iteracoes}, {resolucao.MakespanInicial}, {resolucao.MakespanFinal}, NA");
+                }
+
+                writer.WriteLine("\n");
+
+                //Gera dados para melhor melhora, refazendo paramentros aleatorios por execução
                 for (int i = 0; i <= 10; i++)
                 {
 
@@ -36,6 +54,24 @@ namespace ProblemaTarefas_TeoriaDaComputacao
                     resolucao.ExecutaMelhorEscolha();
 
                     writer.WriteLine($"Monótona melhor escolha, {n}, {m}, {i}, {resolucao.Tempo} ms, {resolucao.Iteracoes}, {resolucao.MakespanInicial}, {resolucao.MakespanFinal}, NA");
+                }
+
+                writer.WriteLine("\n");
+
+                //Gera dados para BLM randomizada, refazendo paramentros aleatorios por execução
+                for (int i = 0; i <= 10; i++)
+                {
+
+                    int m = conjuntoM[rand.Next(0, 3)];
+                    double r = conjuntoR[rand.Next(0, 2)];
+                    double alpha = conjuntoAlpha[rand.Next(0, 9)]; //Define a chance de movimento randomizado
+
+                    int n = (int)Math.Pow(m, r); 
+
+                    ResolucaoTarefas resolucao = new ResolucaoTarefas(n, m, alpha);
+                    resolucao.ExecutaBuscaMonotonaRandomizada();
+
+                    writer.WriteLine($"BLM randomizada, {n}, {m}, {i}, {resolucao.Tempo} ms, {resolucao.Iteracoes}, {resolucao.MakespanInicial}, {resolucao.MakespanFinal}, {resolucao.parametroAlpha}");
                 }
 
             }
--- ProblemaTarefas-TeoriaDaComputacao/ResolucaoTarefas.cs
+++ ProblemaTarefas-TeoriaDaComputacao/ResolucaoTarefas.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Security;
 
 namespace ProblemaTarefas_TeoriaDaComputacao
 {
@@ -8,12 +9,13 @@ namespace ProblemaTarefas_TeoriaDaComputacao
         public int Iteracoes { get; private set; } = 0;
         public int MakespanInicial { get; private set; }
         public int MakespanFinal { get; private set; }
+        public double parametroAlpha { get; set; }
 
         private Maquina[] _maquinas = new Maquina[1];
 
         private Maquina _maquinaMakespan = new Maquina();
 
-        public ResolucaoTarefas(int tarefas, int maquinas)
+        public ResolucaoTarefas(int tarefas, int maquinas, double alpha = 0)
         {
             Tempo = 0;
             Iteracoes = 0;
@@ -21,10 +23,12 @@ namespace ProblemaTarefas_TeoriaDaComputacao
             for(int i = 0; i < maquinas; i++)
                 _maquinas[i] = new Maquina();
             _maquinaMakespan = _maquinas[0];
+            parametroAlpha = alpha;
 
             GerarTarefas(tarefas);
         }
 
+        // Gera n tarefas de tempo aleatório na maquina inicial
         private void GerarTarefas(int n)
         {
             Random rand = new Random();
@@ -35,40 +39,6 @@ namespace ProblemaTarefas_TeoriaDaComputacao
                 _maquinas[0].AdicionarTarefa(t);
             }
             MakespanInicial = _maquinas[0].Makespan;
-        } 
-
-        public void RedefineMaquinaMakespan()
-        {
-            Maquina? maquinaSpan = null;
-            foreach (var maquina in _maquinas)
-            {
-                if(maquinaSpan == null || maquina.Makespan > maquinaSpan.Makespan)
-                    maquinaSpan = maquina;
-            }
-            _maquinaMakespan = maquinaSpan!;
-        }
-
-        public Maquina? RetornarPrimeiroVizinho()
-        {
-            int i = _maquinas.IndexOf(_maquinaMakespan);
-            foreach (var maquina in _maquinas)
-            {
-                if ( maquina.Makespan + _maquinaMakespan.Tarefas.Peek() < _maquinaMakespan.Makespan)
-                    return maquina;
-            }
-            return null;
-        }
-
-        public Maquina? RetornarMelhorVizinho()
-        {
-            Maquina? melhorVizinho = null;
-            foreach (var maquina in _maquinas)
-            {
-                if (maquina.Makespan + _maquinaMakespan.Tarefas.Peek() < _maquinaMakespan.Makespan)
-                    if (melhorVizinho == null || maquina.Makespan < melhorVizinho.Makespan)
-                        melhorVizinho = maquina;
-            }
-            return melhorVizinho;
         }
 
         public void ExecutaPrimeiraEscolha()
@@ -78,20 +48,20 @@ namespace ProblemaTarefas_TeoriaDaComputacao
 
             while (continuar)
             {
-                Maquina? primeiraEscolha = RetornarPrimeiroVizinho();
+                Maquina? primeiraEscolha = RetornarPrimeiroVizinho(); //Seleciona sempre primeiro vizinho elegível encontrado
 
                 if (primeiraEscolha != null)
                 {
                     int tarefa = _maquinaMakespan.RetiraTarefa();
                     primeiraEscolha!.AdicionarTarefa(tarefa);
-                    Iteracoes++;
                     RedefineMaquinaMakespan();
                 }
-                else
+                else // Finaliza se não houver vizinho elegível
                 {
                     continuar = false;
                     MakespanFinal = _maquinaMakespan.Makespan;
                 }
+                Iteracoes++;
             }
             sw.Stop();
             Tempo = sw.Elapsed.TotalMilliseconds;
@@ -104,13 +74,12 @@ namespace ProblemaTarefas_TeoriaDaComputacao
 
             while (continuar)
             {
-                Maquina? melhorEscolha = RetornarMelhorVizinho();
+                Maquina melhorEscolha = RetornarMelhorVizinho(); // Seleciona vizinho com menor makespan
 
-                if (melhorEscolha != null)
+                if (MaquinaElegivel(melhorEscolha))
                 {
                     int tarefa = _maquinaMakespan.RetiraTarefa();
                     melhorEscolha!.AdicionarTarefa(tarefa);
-                    Iteracoes++;
                     RedefineMaquinaMakespan();
                 }
                 else
@@ -118,9 +87,102 @@ namespace ProblemaTarefas_TeoriaDaComputacao
                     continuar = false;
                     MakespanFinal = _maquinaMakespan.Makespan;
                 }
+                Iteracoes++;
             }
             sw.Stop();
             Tempo = sw.Elapsed.TotalMilliseconds;
         }
+
+        public void ExecutaBuscaMonotonaRandomizada()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            Random rand = new Random();
+            bool continuar = true;
+
+            int nIteracoesSemMelhora = 0;
+
+            while (continuar)
+            {
+                Maquina vizinho;
+                if (rand.NextDouble() < parametroAlpha)
+                    vizinho = RetornarVizinhoAleatorio(rand); // Vizinho aleatório
+                else
+                    vizinho = RetornarMelhorVizinho(); // Melhor melhora
+
+                if (MaquinaElegivel(vizinho))
+                {
+                    int tarefa = _maquinaMakespan.RetiraTarefa();
+                    vizinho!.AdicionarTarefa(tarefa);
+                    RedefineMaquinaMakespan();
+                    nIteracoesSemMelhora = 0;
+                }
+                else
+                {
+                    nIteracoesSemMelhora++;
+                    if (nIteracoesSemMelhora >= 1000) // Critério de parada: 1000 iterações sem melhoria
+                    {
+                        continuar = false;
+                        MakespanFinal = _maquinaMakespan.Makespan;
+                    }
+                }
+                Iteracoes++;
+            }
+            sw.Stop();
+            Tempo = sw.Elapsed.TotalMilliseconds;
+        }
+
+        private bool MaquinaElegivel(Maquina maquina) // Verifica se a máquina pode receber a tarefa do makespan sem aumentar o makespan geral
+        {
+            if (maquina.Makespan + _maquinaMakespan.Tarefas.Peek() < _maquinaMakespan.Makespan)
+                return true;
+            return false;
+        }
+
+        private void RedefineMaquinaMakespan() // Define a máquina com maior makespan
+        {
+            Maquina? maquinaSpan = null;
+            foreach (var maquina in _maquinas)
+            {
+                if(maquinaSpan == null || maquina.Makespan > maquinaSpan.Makespan)
+                    maquinaSpan = maquina;
+            }
+            _maquinaMakespan = maquinaSpan!;
+        }
+
+        private Maquina? RetornarPrimeiroVizinho() // Retorna o primeiro vizinho elegível encontrado
+        {
+            int i = _maquinas.IndexOf(_maquinaMakespan);
+            foreach (var maquina in _maquinas)
+            {
+                if (MaquinaElegivel(maquina)) // Realiza a verificação de elegibilidade antecipadamente para não encerrar de forma precosse
+                    return maquina;
+            }
+            return null;
+        }
+
+        private Maquina RetornarMelhorVizinho()
+        {
+            Maquina? melhorVizinho = null;
+            foreach (var maquina in _maquinas)
+            {
+                if (maquina != _maquinaMakespan && (melhorVizinho == null || maquina.Makespan < melhorVizinho.Makespan))
+                    melhorVizinho = maquina;
+            }
+            return melhorVizinho!; // Vizinho com menor makespan
+        }
+
+        private Maquina RetornarVizinhoAleatorio(Random rand) // Retorna aleatoriamente qualquer máquina que não seja a máquina do makespan
+        {
+            int indexSpan = _maquinas.IndexOf(_maquinaMakespan);
+
+            int indexRand = rand.Next(0, _maquinas.Length - 1); // TAM - 1 para returar posição da maquina de Makespan
+
+            if(indexRand >= indexSpan) // Incrementa caso o índice for acima da máquina do makespan
+                indexRand++;
+
+            return _maquinas[indexRand];
+        }
+
+        
     }
 }
